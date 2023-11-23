5a3e91c8bc850dd2fb387028c9d27e6c4798a282
Aman <pawaraman290@gmail.com>
2023-11-23 19:13:18 +0530

    #7 functional Component | what is component

---
+++ src/User.js
@@ -0,0 +1,10 @@
+export function User() {
+  return (
+    <div>
+      <h1>User Component</h1>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+// export default User;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import Users from "./Users";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Users />
+    {/* <Users /> */}
+    <App />
   </React.StrictMode>
 );
 
