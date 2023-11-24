2be78f213bd4011765046af19ca7c4a7253e4f00
Aman <pawaraman290@gmail.com>
2023-11-25 03:34:44 +0530

    #12 State with class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
-import { useState } from "react";
-function App() {
-  const [data, setData] = useState("Aman");
-  function updateData() {
-    setData("aman pawar");
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: "aman",
+    };
   }
-  return (
-    <div className="App">
-      <h1>{data}</h1>
-      <button onClick={updateData}>Update data</button>
-    </div>
-  );
+  apple() {
+    // alert("apple");
+    this.setState({data:"aman pawar"});
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.data}</h1>
+        <button onClick={() => this.apple()}>Update data</button>
+      </div>
+    );
+  }
 }
 
 export default App;
