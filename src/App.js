a605bb2b36a3f10d1d48565044eb2a50be4684a8
Aman <pawaraman290@gmail.com>
2023-11-25 02:50:40 +0530

    function updateData() {

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
-  let myName = "aman pawar";
-  function apple() {
-    myName = "rjt";
-    alert(myName);
+  const [data, setData] = useState("Aman");
+  function updateData() {
+    setData("aman pawar");
   }
   return (
     <div className="App">
-      <h1>{myName}</h1>
-      <button onClick={apple}>Click me!</button>
+      <h1>{data}</h1>
+      <button onClick={updateData}>Update data</button>
     </div>
   );
 }
