9131631e0289ecfa8a2d1853bc2b2b5b5ba3d32e
Aman <pawaraman290@gmail.com>
2023-11-25 14:50:02 +0530

    #16 Hide , Show and Toggle in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import "./App.css";
 import React, { useState } from "react";
 function App() {
-  const [data, setData] = useState(null);
-  const [print, setPrint] = useState(false);
-  function getData(val) {
-    console.warn(val.target.value);
-    setData(val.target.value);
-    setPrint(false);
-  }
+  const [status, setStatus] = useState(false);
   return (
     <div className="App">
-      {print ? <h1>{data}</h1> : null}
-      <input type="text" onChange={getData} />
-      <button onClick={() => setPrint(true)}>update name</button>
+      {status ? <h1>Hello World !</h1> : null}
+      {/* <button onClick={()=> setStatus(false)}>Hide</button>
+      <button  onClick={()=> setStatus(true)}>Show</button> */}
+      <button onClick={() => setStatus(!status)}>Toggle</button>
     </div>
   );
 }
