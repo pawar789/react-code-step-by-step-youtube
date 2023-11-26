ed4334af43dfcadfef58126e5ffe9acbdd8d095c
Aman <pawaraman290@gmail.com>
2023-11-26 14:17:55 +0530

    #24 componentDidMount life cycle method in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
 import React, { useState } from "react";
 import Render from "./Render";
-function App() {
-  const [name, setName] = useState("aman");
-  return (
-    <div>
-      <Render name={name} />
-      {/* <button onClick={() => setName("pawar")}>Update me</button> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "aman",
+    };
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  render() {
+    console.log("render");
+    // this.setState({ name: "pawar" })
+    return (
+      <div>
+        <h1>
+          Component Did Mount <span>{this.state.name}</span>
+        </h1>
+        <button onClick={() => this.setState({ name: "pawar" })}>
+          Update Name
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
