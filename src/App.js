7f89e4519495433a975fbd5b5a4fbec43c89d360
Aman <pawaraman290@gmail.com>
2023-11-26 16:12:36 +0530

    #28 Hooks in ReactJs

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import "./App.css";
 import React, { useState } from "react";
 import Render from "./Render";
 import Student from "./Student";
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      show: false,
-    };
-  }
-  render() {
-    return (
-      <div className="App">
-        {this.state.show ? <Student /> : <h1>Child Component Removed</h1>}
-        <button onClick={() => this.setState({ show: !this.state.show })}>
-          Toggle Child Button
-        </button>
-      </div>
-    );
-  }
+function App() {
+  const [data, setData] = useState("Aman");
+  return (
+    <div className="App">
+      <h1>{data}</h1>
+      <button onClick={() => setData("Pawar")}>Update Data</button>
+    </div>
+  );
 }
 
 export default App;
