059d992526221acd81674059349785d142578d62
Aman <pawaraman290@gmail.com>
2023-12-01 01:43:51 +0530

    #33 Array Listing with Map function

--- src/App.js
+++ src/App.js
@@ -4,13 +4,47 @@ import Render from "./Render";
 import Student from "./Student";
 import { User } from "./User";
 import "./style.css";
-import style from './custom.module.css';
+import style from "./custom.module.css";
+import { Button } from "react-bootstrap";
 function App() {
+  // const student = ["Anil", "Sidhu", "Sam", "Peter"];
+  const students = [
+    {
+      name: "anil",
+      email: "anil@gmail.com",
+      contact: 888,
+    },
+    {
+      name: "sidhu",
+      email: "sidhu@gmail.com",
+      contact: 1111,
+    },
+    {
+      name: "peter",
+      email: "peter@gmail.com",
+      contact: 2222,
+    },
+  ];
+
   return (
     <div className="App">
-      <h1 className="primary">Style type 1 in React Js</h1>
-      <h1 style={{color:'red',backgroundColor:'blue'}}>Style type 2 in React Js</h1>
-      <h1 className={style.success }>Style type 2 in React Js</h1>
+      <h1>Handle Array With List</h1>
+      <table border="1">
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact</td>
+        </tr>
+
+        {students.map((data) => (
+          <tr>
+            <td>{data.name}</td>
+            <td>{data.email}</td>
+            <td>{data.contact}</td>
+            <td></td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
