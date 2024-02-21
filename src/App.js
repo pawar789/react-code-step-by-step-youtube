e610031eedf2b46f5bf28840639a14da078d8048
Aman <pawaraman290@gmail.com>
2024-02-21 18:24:03 +0530

    #37 fragment in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import "./App.css";
 import User1 from "./User1";
-import React from "react";
+import React, { Fragment } from "react";
 import { Table } from "react-bootstrap";
+import Cols from "./Cols";
 function App() {
-  const users = [
-    {
-      name: "Anil",
-      email: "anil@test.com",
-      address: "Betul",
-    },
-    {
-      name: "Burce",
-      email: "bruce@test.com",
-      address: "Bhopal",
-    },
-    {
-      name: "Peter",
-      email: "peter@test.com",
-      address: "Indore",
-    },
-    {
-      name: "Sam",
-      email: "sam@test.com",
-      address: "Sarni",
-    },
-  ];
   return (
-    <div className="App">
-      <h1>Reuse Component with List</h1>
-      {users.map((item, i) => (
-        <User1 data={item} />
-      ))}
+    <div>
+      <h1>React Fragment</h1>
+      <table>
+        <tbody>
+          <tr>
+            <Cols />
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
